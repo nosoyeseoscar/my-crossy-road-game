23791fc3f42d0b3265723647ad6000aeb87998e8
Oscar Fuentes <ofuentese@gmail.com>
2025-03-12 15:12:15 -0700

    my progrees so far

---
+++ src/components/Player.ts
@@ -0,0 +1,59 @@
+import * as THREE from "three";
+import type { MoveDirection } from "../types";
+
+export const player = Player();
+
+function Player() {
+  const player = new THREE.Group();
+
+  const body = new THREE.Mesh(
+    new THREE.BoxGeometry(15, 15, 20),
+    new THREE.MeshLambertMaterial({
+      color: "white",
+      flatShading: true,
+    })
+  );
+  body.position.z = 10;
+  body.castShadow = true;
+  body.receiveShadow = true;
+
+  player.add(body);
+
+  const cap = new THREE.Mesh(
+    new THREE.BoxGeometry(2,4,2),
+    new THREE.MeshLambertMaterial({
+      color: 0xf0619a,
+      flatShading: true,
+    })
+  );
+  cap.position.z = 21;
+  cap.castShadow = true;
+  cap.receiveShadow = true;
+  player.add(cap);
+
+  return player;
+
+}
+
+export const position: {
+  currentRow: number;
+  currentTile: number;
+} = {
+  currentRow: 0,
+  currentTile: 0,
+};
+
+export const movesQueue: MoveDirection[] = [];
+
+export function queueMove(direction: MoveDirection) {
+  movesQueue.push(direction);
+}
+
+export function stepCompleted() {
+  const direction = movesQueue.shift();
+
+  if (direction === "forward") position.currentRow += 1;
+  if (direction === "backward") position.currentRow -= 1;
+  if (direction === "left") position.currentTile -= 1;
+  if (direction === "right") position.currentTile += 1;
+}
---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import * as THREE from "three";
+import { Renderer } from "./components/Renderer";
+import { Camera } from "./components/Camera";
+import { DirectionalLight } from "./components/DirectionalLight";
+import { player } from "./components/Player";
+import { map,initializeMap } from "./components/Map";
+import { animateVehicles } from "./animateVehicles";
+import { animatePlayer } from "./animatePlayer";
+import "./style.css";
+import "./collectUserInputs";
+
+function initializeGame() {
+    initializeMap();
+}
+
+function animate() {
+    animateVehicles();
+    animatePlayer();
+    
+    renderer.render(scene, camera);
+}
+
+const scene = new THREE.Scene();
+scene.add(player);
+scene.add(map);
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+const dirLight = DirectionalLight();
+scene.add(dirLight);
+
+/* const dirLight = new THREE.DirectionalLight();
+dirLight.position.set(-100, -100, 200);
+scene.add(dirLight); */
+
+const camera = Camera();
+scene.add(camera);
+
+initializeGame();
+
+const renderer = Renderer();
+renderer.setAnimationLoop( animate )
+//renderer.render(scene, camera);
